7da5e637c8e2e29b3f0536ee3234a95abcfc5721
Jose Narvaez <joseluis.narvaez0@gmail.com>
2023-10-30 02:35:34 -0300

    Correccion de nombre

--- jose-narvaez-tiendaJose/src/App.jsx
+++ jose-narvaez-tiendaJose/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer2.jsx'
 import Menu from './components/Menu/Menu.jsx'
 
 function App() {
